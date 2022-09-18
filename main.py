8c7326d094430c5a0aabbe674f127bf051e2ca19
Maciek Ciupa <>
2022-09-18 13:26:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Welcome to band names generator.")
+city_name = input("In which city do you live?\n")
+animal = input("What animal do you have?\n")
+print(f"Congratulation your band name is: {city_name} {animal}")
